50ebb729f3e0389a57adce4c83b796022386562c
Shirjeel <rajasarjais@gmail.com>
2024-09-10 22:35:31 +0530

    js changes

--- chatting_app/static/js/script.js
+++ chatting_app/static/js/script.js
@@ -2,9 +2,6 @@ console.log('connected');
 const userPk = document.querySelector('input[name="other_user"]').value;
 
 console.log('userpk', userPk);
-// console.log(document.querySelector('.chats').innerHTML += '<div class="chat-message chat-other"><p>' +'youhohohohohohohohohoh'+ '</p></div>')
-    // console.log(document.querySelector('.chats').innerHTML += '<div class="chat-message chat-user"><p>' + 'whofhohfeowhfoh' + '</p></div>')
-
 
 const chatSocket = new WebSocket(
     'ws://' + window.location.host + '/ws/chat/' + userPk + '/'
@@ -21,7 +18,7 @@ chatSocket.onerror = function(e) {
 chatSocket.onmessage = function(e) {
     const data = JSON.parse(e.data);
     console.log('hi')
-    console.log("Received message:", data.message); // Check if this logs
+    console.log("Received message:", data.message);
     document.querySelector('.chats').innerHTML += '<div class="chat-message chat-other"><p>' + data.message + '</p></div>';
 };
 
@@ -32,7 +29,7 @@ chatSocket.onclose = function(e) {
 document.querySelector('.send-button').onclick = function(e) {
     const messageInput = document.querySelector('.message-box');
     const message = messageInput.value;
-    console.log("Sending message:", message);  // Check if this logs
+    console.log("Sending message:", message);  
     console.log('yo')
     document.querySelector('.chats').innerHTML += '<div class="chat-message chat-user"><p>' + message + '</p></div>';
 
@@ -41,5 +38,4 @@ document.querySelector('.send-button').onclick = function(e) {
     chatSocket.send(JSON.stringify({
         'message': message
     }));
-    // messageInput.value = '';
 };
